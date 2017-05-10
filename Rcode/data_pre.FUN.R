e45bed56dcdc6e75d8744e86816244e4e8e2d7eb
ag300g <ag300g@163.com>
2017-05-10 17:02:14 +0800

    commit

---
+++ Rcode/data_pre.FUN.R
@@ -0,0 +1,10 @@
+#' @description data preparation
+#' @param sku_sku all the sku and sku relationship
+#' @param sku_store all sku and store relationship
+#' @param NO_store the number of the store be chosen
+#' @return a list:glpk_param include all the params glpk used
+#' @return a list:ori_net include 1, sku_sku network from the NO_store 2, 
+
+data_pre <- function(sku_sku,sku_store,NO_store){
+  
+}
