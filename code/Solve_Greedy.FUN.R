324577ab841f8a2cc1e868c1572d927ca77701f1
ag300g <ag300g@163.com>
2017-05-12 10:38:46 +0800

    commit

---
+++ code/Solve_Greedy.FUN.R
@@ -0,0 +1,6 @@
+#' @description solve the subproblem using greedy algrithm
+#' @param p the corresponding sku name and network'
+#' @param NO_store the store number
+Solve_Greedy <- function(){
+  
+}
--- code/main.R
+++ code/main.R
@@ -7,4 +7,5 @@ source(file="code/Data_Process.FUN.R",encoding="utf-8")
 storelist
 NO_store <- 157
 
-a <- Data_Process(sku_sku,sku_store,NO_store)
+p <- Data_Process(sku_sku,sku_store,NO_store)
+r <- Solve_Glpk(p[[1]],p[[2]],NO_store)
