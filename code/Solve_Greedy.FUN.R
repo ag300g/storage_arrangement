357103509065a6249a4e8d2cee01d209c57ed951
ag300g <ag300g@163.com>
2017-05-14 22:59:39 +0800

    commit

--- code/Solve_Greedy.FUN.R
+++ code/Solve_Greedy.FUN.R
@@ -1,6 +1,6 @@
 #' @description solve the subproblem using greedy algrithm
 #' @param p the corresponding sku name and network'
 #' @param NO_store the store number
-Solve_Greedy <- function(){
+Solve_Greedy <- function(little_sku_sku,sotrelist){
   
 }
