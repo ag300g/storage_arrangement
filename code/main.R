45b09015a409201b1ead455ac1e08f3b50864b59
liuyinliang1 <liuyinliang@jd.com>
2017-05-19 18:27:05 +0800

    commit

--- code/main.R
+++ code/main.R
@@ -7,8 +7,16 @@ source(file="code/Solve_Glpk.FUN.R",encoding="utf-8")
 source(file="code/Solve_Greedy.FUN.R",encoding="utf-8")
 
 storelist
-NO_store <- 75
+store <- storelist$name[storelist$sku_NUM > 50 & storelist$sku_NUM < 1000]
 
-p <- Data_Process(sku_sku,sku_store,NO_store)
-glpkresult <- Solve_Glpk(p$p1,p$p2)
-greedyresult <- Solve_Greedy(p$p2)
+comp <- list()
+for (i in 1:length(store))
+{ 
+  NO_store <- store[i]
+  p <- Data_Process(sku_sku,sku_store,NO_store)
+  glpkresult <- Solve_Glpk(p$p1,p$p2)
+  greedyresult <- Solve_Greedy(p$p2)
+  comp[[i]] <- list(glpkresult=glpkresult,greedyresult=greedyresult)
+}
+names(comp) <- store
+save(comp,file="./data/compare.RData")
